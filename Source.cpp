d9fc1b1cee8ca1ec0328a4295868c3328e20a480
sakai0202 <51104109+sakai0202@users.noreply.github.com>
2020-06-04 15:59:07 +0900

    別サンプルを参考に修正

--- Source.cpp
+++ Source.cpp
@@ -5,111 +5,79 @@
 
 using namespace::std;
 
-class Student {
+class Book {
 private:
     string name;
-    int sex; //男の子:1  女の子:2
 public:
-    //コンストラクタ
-    Student(string na, int se) {
-        name = na;
-        sex = se;
+    Book(string name) {
+        this->name = name;
     }
 
-    //コピーコンストラクタ
-    Student(const Student& stu ) {
-        name = stu.name;
-        sex = stu.sex;
+    Book(const Book& boo) {
+        this->name = boo.name;
     }
 
     string getName() {
-        return name;
-    }
-    int getSex() {
-        return sex;
-    }
-};
-
-class StudentList {
-protected:
-    vector<Student> students;
-public:
-    StudentList() {};
-
-    void add(Student student) {
-        students.push_back(student);
-    }
-
-    Student getStudentAt(int index) {
-        return students[index];
-    }
-
-    int getSize() {
-        return students.size();
-    }
-};
-
-class Teacher{
-protected:
-    StudentList studentList;
-public:
-    void createStudentList();
-    void callStudent();
-};
-
-class MyTeacher : public Teacher{
-private:
-    StudentList* studentList;
-public:
-    MyTeacher() {
-        this->studentList = new StudentList();
-    }
-
-    void createStudentList() {
-        //StudentList* studentlist = new StudentList();
-        this->studentList->add(Student("赤井亮太", 1)); //Same Mean↓
-
-//        Student stu1("赤井亮太", 1);
-//        list->add(stu1);
-
-        this->studentList->add(Student("赤羽里美", 2));
-        this->studentList->add(Student("岡田澪", 2));
-        this->studentList->add(Student("西森俊介", 1));
-        this->studentList->add(Student("中の森玲奈", 2));
-    }
-
-    void callStudent() {
-        int size = studentList->getSize();
-
-        for (int i = 0; i < size; i++) {
-            cout << studentList->getStudentAt(i).getName() << endl;
-        }
+        return this->name;
     }
 };
 
 class Iterator {
-    bool hasNext();
-    virtual Student next();
+public:
+    virtual bool hasNext() =0;
+    virtual Book next() =0;
 };
 
 class Aggregate {
-    Iterator iterator();
+public:
+    virtual Iterator * myIterator() =0;
+    virtual ~Aggregate() {};
 };
 
-class MyStudentListIterator : public Iterator {
+class BookShelfIterator;
 
+class BookShelf :public Aggregate {
 private:
-    MyStudentList* myStudentList;
-    int index;
+    vector<Book> books;
 public:
-    //コンストラクタ
-    MyStudentListIterator(MyStudentList* list) {
-        this->myStudentList = list;
+
+    void addBook(const Book& newbook) {
+        books.push_back(newbook);
+    }
+
+    Book getBookAt(int index) {
+        return books[index];
+    }
+
+    int getLength() {
+        return books.size();
+    }
+
+    Iterator* myIterator();
+    
+};
+
+class BookShelfIterator :public Iterator {
+private:
+    BookShelf bookShelf;
+    int index;
+
+public:
+    BookShelfIterator(BookShelf newBookShelf) {
+        this->bookShelf = newBookShelf;
         this->index = 0;
     }
 
+    //ポインタを受け取った時のコンストラクタ
+    //これがないとIterator *p = new MenuBookIterator(this);でエラー
+    explicit BookShelfIterator(BookShelf* newBookShelf) {
+        this->bookShelf = *newBookShelf;
+        this->index = 0;
+    }
+
+
     bool hasNext() {
-        if (this->myStudentList->getSize() > index) {
+        if (index < bookShelf.getLength()) {
             return true;
         }
         else {
@@ -117,45 +85,34 @@ public:
         }
     }
 
-    Student next() override {
-        Student s = myStudentList->getStudentAt(index);
+    Book next() override {
+        Book book = bookShelf.getBookAt(index);
         index++;
-        return s;
+        return book;
     }
 };
 
-class MyStudentList : public StudentList, public Aggregate {
-public:
-    MyStudentList() {
-    }
-
-    Iterator iterator() {
-        return MyStudentListIterator(this);
-    }
-};
-
-class VeteranTeacher :public Teacher {
-    private MyStudentList list;
-
-    public void createStudentList() {
-        list = new MyStudentList(3);
-        list.add(new Student("中川雄介", 1));
-        list.add(new Student("板東恵美", 2));
-        list.add(new Student("水島太郎", 1));
-    }
-
-    public void callStudents() {
-        Iterator itr = list.iterator();
-        while (itr.hasNext()) {
-            System.out.println(((Student)itr.next()).getName());
-        }
-    }
+Iterator* BookShelf::myIterator(){
+    Iterator* p = new BookShelfIterator(this);
+    return p;
+    //return BookShelfIterator(this);
 }
 
 
 int main() {
-    MyTeacher* you = new MyTeacher();
-    you->createStudentList();
-    you->callStudent();
+    BookShelf *bookShelf = new BookShelf();
+
+    bookShelf->addBook(Book("Git Hub"));
+    bookShelf->addBook(Book("Qiita"));
+    bookShelf->addBook(Book("Java"));
+    bookShelf->addBook(Book("Iterator pattern"));
+
+    Iterator* iterator = bookShelf->myIterator();
+
+    while (iterator->hasNext()) {
+        Book book = (Book)iterator->next();
+        cout << book.getName() << endl;
+    }
 }
+
 
